d3196f8ca5071cabf7703b07d59bdaff88c1f228
gaspard C <gaspard@courchinoux.org>
2022-07-08 20:56:48 +0200

    ajout des structures de pagination
    faire la résolvation des adresses

--- gaspardasm/Memory.h
+++ gaspardasm/Memory.h
@@ -31,3 +31,68 @@ public:
 	void set_memory(int byte);
 };
 
+struct pag_general_config {
+
+	unsigned short pag_table_num; 
+	unsigned char num_pag_child; 
+	unsigned char child_size; 
+
+	/*
+	1= 512 bytes 
+
+	2 = 1 kilobytes 
+
+	3 = 1 mégabytes 
+	
+	*/
+
+
+};
+
+struct page_table {
+
+
+	unsigned char mode : 4; 
+	/*
+	1 = kernel mode 
+	2 only acees in kernel m 
+	3 = access in both mode 
+	*/
+	unsigned char disabled : 4; 
+	/*
+	1 page cannot be writed or read
+
+	2 this page can only be read 
+	3 rthis page can only be writed
+	*/
+	unsigned char present : 1; 
+	/*
+	doit être présent pour être effectif
+	*/
+
+	struct page_child child[];
+};
+struct page_child {
+
+	unsigned char is_writed:1; 
+
+	unsigned char mode : 4; 
+	/*
+	1= kernel mode
+	2= uniquement accès user mode 
+	3 = accès dans les deux 
+	*/
+
+	unsigned char disabled : 4; 
+	/*
+	1 = impossible d'écrire
+	2= peut uniquement être lu 
+	3= peut uniquement être écrit
+	*/
+
+	unsigned long long adress; 
+	/*
+	adresse physique qui est paginée 
+	*/
+
+};
